f3bc15a200c0d301974a0b400f66a5ca6da397af
lsstanley <55392841+lsstanley@users.noreply.github.com>
2023-11-25 22:16:45 +0000

    updated getProduct.php, works with /getProduct.php

--- getProduct.php
+++ getProduct.php
@@ -5,16 +5,16 @@
     $category = $_GET['product'];
     $response = "";
 
-    try{
-        $query = "SELECT ProductID, Price, ProductImage FROM PRODUCTS WHERE `Type` = 'Vodka'";
+    try {
+        $query = "SELECT ProductID, Price, ProductImage FROM PRODUCTS WHERE ProductType = $category";
         $stmt = $mysql->prepare($query);
         $stmt->execute();
         $result = $stmt->fetchAll();
-        foreach($result as $row){
-            $response += $row['ProductName'].",".$row['Price'].",".$row['ProductImage'].":";
+        foreach($result as $row) {
+            $response .= $row['ProductID'].",".$row['Price'].",".$row['ProductImage'].":"."\n"; // Use .= for string concatenation
         }
         echo $response;
-    } catch (PDOException $e){
-        echo $sql . "<br>" . $e->getMessage();
+    } catch (PDOException $e) {
+        echo $query . "<br>" . $e->getMessage(); 
     }
 ?>
