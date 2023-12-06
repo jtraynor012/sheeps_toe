c25706a35cd1ff92d2148b89a18549ab719c9d78
jtraynor012 <190010800@dundee.ac.uk>
2023-12-06 21:39:35 +0000

    fixed getPastOrders

--- getPastOrders.php
+++ getPastOrders.php
@@ -6,19 +6,18 @@
     $customer = false;
 
     try{
+        //if a customer wants their previous orders
         if(isset($_GET['c'])){
             $query = "SELECT * FROM OrderSummary WHERE CustomerID = :custID";
+            $stmt = $mysql->prepare($query);
+            $stmt->bindParam(":custID", $_SESSION['id'], PDO::PARAM_INT);
             $customer = true;
         }
+        //if manager wants previous orders for branch
         else{
             $query = "SELECT * FROM OrderSummary WHERE BranchID = :branchID";
-        }
-
-        $stmt = $mysql->prepare($query);
-        $stmt->bindParam(":branchID", $branchID, PDO::PARAM_INT);
-
-        if($customer){
-            $stmt->bindParam(":custID", $_SESSION['id'], PDO::PARAM_INT);
+            $stmt = $mysql->prepare($query);
+            $stmt->bindParam(":branchID", $branchID, PDO::PARAM_INT);
         }
         $stmt->execute();
         $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
